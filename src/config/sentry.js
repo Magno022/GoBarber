3ab7fc2619f0ddbdc7dc377f95d38ea34e864c2f
Magno022 <59201054+Magno022@users.noreply.github.com>
2020-01-09 10:58:28 -0300

    Add files via upload

---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://4c9989c88a4644c68f7ba391cdead165@sentry.io/1875246',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,39 @@
+import multer from 'multer';
 import { Router } from 'express';
-import UserController from './app/models/UserController';
+import UserController from './app/controller/UserController';
+import SessionController from './app/controller/SessionController';
+import FileController from './app/controller/FileController';
+import ProviderController from './app/controller/ProviderController';
+import AppointmentController from './app/controller/AppointmentController';
+import AvailableController from './app/controller/AvailableController';
+import NotificationController from './app/controller/NotificationController';
+import ScheduleController from './app/controller/ScheduleController';
+import authMiddleware from './app/middlewares/alth';
+import multerConfig from './config/multer';
+
 
 const routes = new Router();
+const upload = multer(multerConfig);
+
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), FileController.store);
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+routes.get('/schedules', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
 export default routes;
+
+/** 
+ * sudo lsof -i :3333
+   sudo kill -9 PID_DO_PROCESSO
+/** */
